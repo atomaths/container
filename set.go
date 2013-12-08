627fed5cefd389605ec53104499faf58dd808105
Jongmin Kim <atomaths@gmail.com>
2013-12-08 23:08:02 +0900

    initial commit

---
+++ set.go
@@ -0,0 +1,13 @@
+// Copyright 2013 Jongmin Kim. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package set implements the Set data structure in Java or C++.
+
+package set
+
+type Element struct {
+}
+
+func New() *Element {
+}
